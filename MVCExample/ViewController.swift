3630877857153e98b5f181e5bcdf94c0d2c5fe76
Çağatay Emekci <cagatayemekci@gmail.com>
2018-10-21 17:26:08 +0300

    Initial Commit

---
+++ MVCExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVCExample
+//
+//  Created by Çağatay Emekci on 21.10.2018.
+//  Copyright © 2018 Çağatay Emekci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
